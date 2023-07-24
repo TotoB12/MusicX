c3acd9401c6f583a625d450924aae438da4c0319
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-07-24 12:16:37 -0400

    Delete src/Tests directory

--- src/Tests/Sandbox/Options/InitialSeedOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sandbox.Options
-{
-    using CommandLine;
-
-    [Verb("initialize", HelpText = "Initial seed of data for the web application.")]
-    public class InitialSeedOptions
-    {
-    }
-}
--- src/Tests/Sandbox/Options/SandboxOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sandbox.Options
-{
-    using CommandLine;
-
-    [Verb("sandbox", HelpText = "Run sandbox code.")]
-    public class SandboxOptions
-    {
-    }
-}
